41efbb36b267b55103b4dfd1d86c21d01d3a783e
Mithil S Kandakur <m.kandkur@icloud.com>
2026-01-20 15:05:31 +0530

    Update groq_day1.py

--- groq_day1.py
+++ groq_day1.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from groq import Groq
 
-st.set_page_config("PragyanAI Content Generator", layout="wide")
-st.title("📢 PragyanAI – Content Generator")
+st.set_page_config("MitsAI Content Generator", layout="wide")
+st.title("📢 Mits – Content Generator")
 
 client = Groq(api_key=st.secrets["GROQ_API_KEY"])
 
